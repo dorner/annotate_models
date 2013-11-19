f6a54750d9829f3a1bbc04db5bd34cfe73161874
Daniel Orner <daniel.orner@wishabi.com>
2013-11-19 13:28:32 -0500

    Fix for skip_on_db_migrate

--- lib/annotate.rb
+++ lib/annotate.rb
@@ -23,6 +23,7 @@ module Annotate
     :show_indexes, :simple_indexes, :include_version, :exclude_tests,
     :exclude_fixtures, :exclude_factories, :ignore_model_sub_dir,
     :format_bare, :format_rdoc, :format_markdown, :sort, :force, :trace,
+    :skip_on_db_migrate
   ]
   OTHER_OPTIONS=[
     :model_dir,
